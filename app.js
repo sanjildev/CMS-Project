eca047fcfdef0c1cff34824d5832fb53d98670d5
sanjildev <shakyasanjil00@gmail.com>
2026-06-09 06:43:29 +0545

    add blogs,single blogs and create blogs ai successfully

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const app=require('express')()
+const express=require('express')
+const app=express()
 
 const { connectDatabase } = require('./database/database')
+const Blog = require('./model/blogModel')
+
+
+app.use(express.json()) //node js lai form baata aako data buja vaneko
+app.use(express.urlencoded({extended:true}))
+
 
 //database connection
 connectDatabase()
@@ -11,6 +18,71 @@ app.get('/',(req,res)=>{
     })
 })
 
+
+//Get Blog API
+
+
+app.get('/blogs',async(req,res)=>{
+const blogs=await Blog.find()
+if(blogs.length==0){
+res.json({
+    status:404,
+    message:"empty blogs"
+})    
+}
+else{
+res.json({
+    status:200,
+    blogs,
+    message:"all blogs fetched successfully"
+})
+}
+})
+
+
+//Single GET Blog API
+
+
+app.get('/blog/:id',async(req,res)=>{
+const id=req.params.id
+const blog=await Blog.findById(id)
+if(!blog){
+res.json({
+    status:404,
+    message:"no blog found"
+})
+}
+else{
+res.json({
+    status:200,
+    blog,
+    message:`${blog.title} blog fetched successfully`
+})
+    
+}  
+
+})
+//CREATE BLOG API
+
+app.post('/createBlog',async(req,res)=>{
+    const title=req.body.title
+    const subtitle=req.body.subtitle
+    const description=req.body.description
+
+    //alternative
+
+   // const {title,subtitle,description}=req.body
+    //Insert to database
+    await Blog.create({
+        title,
+        subtitle,
+        description
+    })
+    res.json({
+        status:201,
+        message:"Blog created successfully !!"
+    })
+})
 
 app.listen(3000,(req,res)=>{
     console.log('Server is running in 3000 port');
---
+++ model/blogModel.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose')
+const Schema=mongoose.Schema
+ const blogSchema=new Schema({
+    title:{
+        type:String,
+    },
+    subtitle:{
+        type:String
+    },
+    description:{
+        type:String
+    }
+ },{
+    timestamps:true
+ })
+
+ const Blog=mongoose.model('Blog',blogSchema)
+
+ module.exports=Blog
